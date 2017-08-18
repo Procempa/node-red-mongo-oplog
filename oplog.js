45a720b83df5a191d726eb0a683e6f1e2e2207ae
Derotino Fraga da Silveira Junior <derotino.junior@procempa.com.br>
2017-08-18 18:33:26 -0300

    Remove mongo-oplog npm package

--- oplog.js
+++ oplog.js
@@ -1,22 +1,34 @@
 module.exports = function(RED) {
-	var MongoOplog = require('mongo-oplog');
+	'use strict';
+	//var MongoOplog = require('mongo-oplog');
 	var mongoClient = new require('mongodb').MongoClient();
 	var _ = require('lodash');
+	var mustache = require('mustache');
+	var Timestamp = require('mongodb').Timestamp;
 
-	function MongoOplogNode(config) {
-		RED.nodes.createNode(this, config);
+	function _setEnv(config) {
+		var result = [];
+		for (var key in config) {
+			if (/{{/.test(config[key])) {
+				result[key] = mustache.render(config[key], process.env);
+			} else {
+				result[key] = config[key];
+			}
+		}
+		return result;
+	}
+
+	function MongodbOplogNode(n) {
+		var config_env = _setEnv(n);
+		RED.nodes.createNode(this, config_env);
 		var node = this;
-		this.oplog_url = config.oplog_url;
-		this.mongo_url = config.mongo_url;
-		this.ns = config.ns;
-
-		this.oplog = MongoOplog(this.oplog_url, { ns: this.ns });
-
-		this.oplog.tail();
+		this.oplog_url = config_env.oplog_url;
+		this.mongo_url = config_env.mongo_url;
+		this.ns = config_env.ns;
 
 		var sendUpsert = function(doc) {
 			var id = _.get(doc, 'o2._id');
-			var collection = _.tail(_.split(this.ns, '.'));
+			var collection = _.last(_.split(this.ns, '.'));
 			mongoClient.connect(this.mongo_url).then(function(db) {
 				db.collection(collection).findOne({ _id: id }).then(function(o) {
 					var msg = {
@@ -38,15 +50,70 @@ module.exports = function(RED) {
 			node.send(msg);
 		};
 
-		this.oplog.on('insert', sendUpsert.bind(this));
-		this.oplog.on('update', sendUpsert.bind(this));
-		this.oplog.on('delete', sendDeleted.bind(this));
+		var streamObserver = function(data) {
+			console.log(data);
+			switch (data.op) {
+				case 'i':
+				case 'u':
+					sendUpsert.apply(this, [data]);
+					break;
+				case 'd':
+					sendDeleted.apply(this, [data]);
+					break;
+			}
+		};
+
+		var lastDoc = function(doc) {
+			var query = {
+				ns: this.ns,
+				ts: {
+					$gt: doc.ts || Timestamp(0, (Date.now() / 1000) | 0)
+				}
+			};
+			this.oplog = this.localDB
+				.collection('oplog.rs')
+				.find(query, {
+					tailable: true,
+					awaitData: true,
+					oplogReplay: true,
+					noCursorTimeout: true,
+					numberOfRetries: Number.MAX_VALUE
+				})
+				.stream();
+
+			this.oplog.on('data', streamObserver.bind(this));
+		};
+
+		var processConnection = function(db) {
+			this.localDB = db;
+			db
+				.collection('oplog.rs')
+				.find({}, { ts: 1 })
+				.sort({ $natural: -1 })
+				.limit(1)
+				.nextObject()
+				.then(lastDoc.bind(this));
+		};
+
+		var startObserve = function() {
+			try {
+				mongoClient.connect(this.oplog_url).then(processConnection.bind(this));
+			} catch (err) {
+				this.error(err, msg);
+			}
+		};
 
 		var onClose = function() {
-			this.oplog.destroy();
+			this.oplog.close();
+			this.localDB.close();
 		};
-		this.on('close', onClose.bind(this));
-	}
 
-	RED.nodes.registerType('mongodb-oplog', MongoOplogNode);
+		this.on('input', startObserve.bind(this));
+		this.on('close', onClose.bind(this));
+
+		setTimeout(function() {
+			node.emit('input', {});
+		}, 100);
+	}
+	RED.nodes.registerType('mongodbOplog', MongodbOplogNode);
 };
